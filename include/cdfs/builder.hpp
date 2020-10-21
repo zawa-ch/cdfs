18bb52933af389b711d9f91a073985c069d3b214
zawa-ch <satellite.2e1834097@gmail.com>
2020-10-21 15:46:57 +0900

    + CDFSBuilder

---
+++ include/cdfs/builder.hpp
@@ -0,0 +1,30 @@
+#ifndef __cdfs_builder__
+#define __cdfs_builder__
+#include <array>
+#include <iostream>
+#include "cdfs.hpp"
+namespace zawa_ch::CDFS
+{
+	class CDFSBuilder
+	{
+	public:
+		typedef std::array<uint8_t, 240> ContainsType;
+	private:
+		std::string label;
+		UInt128 frameindex;
+		UInt128 datasize;
+	public:
+		CDFSBuilder();
+		CDFSBuilder(const std::string& label);
+
+		void WriteHEADFrame(std::ostream& stream);
+		void WriteHEADFrame(std::ostream& stream, const UInt128& framecount, const UInt128& datasize);
+		void WriteFINFFrame(std::ostream& stream);
+		void WriteDATAFrame(std::ostream& stream, const ContainsType& data);
+		void WriteCONTFrame(std::ostream& stream);
+
+		static void WriteToStream(std::ostream& stream, const CDFSFrame& frame);
+		static void SetCRC(CDFSFrame& frame);
+	};
+}
+#endif // __cdfs_builder__
---
+++ src/builder.cpp
@@ -0,0 +1,82 @@
+#include "cdfs/builder.hpp"
+using namespace zawa_ch::CDFS;
+
+CDFSBuilder::CDFSBuilder() {}
+CDFSBuilder::CDFSBuilder(const std::string& label) : label(label) {}
+
+void CDFSBuilder::WriteHEADFrame(std::ostream& stream) { WriteHEADFrame(stream, frameindex + 1, datasize); }
+void CDFSBuilder::WriteHEADFrame(std::ostream& stream, const UInt128& framecount, const UInt128& datasize)
+{
+	CDFSHEADFrame frame;
+	frame.seq = uint64_t(frameindex);
+	frame.frametype = CDFSFrameTypes::HEAD;
+	frame.data_formatver = CDFS::FormatVersion;
+	frame.data_count = framecount;
+	{
+		auto si = label.cbegin();
+		auto di = frame.data_label.begin();
+		auto se = label.cend();
+		auto de = frame.data_label.end();
+		while((si != se)&&(di != de)) { *(di++) = *(si++); }
+	}
+	frame.data_size = datasize;
+	SetCRC(CDFS::CastToFrame(frame));
+	WriteToStream(stream, CDFS::CastToFrame(frame));
+	++frameindex;
+}
+void CDFSBuilder::WriteFINFFrame(std::ostream& stream)
+{
+	CDFSFINFFrame frame;
+	frame.seq = uint64_t(frameindex);
+	frame.frametype = CDFSFrameTypes::FINF;
+	frame.data_count = frameindex + 1;
+	// TODO: チェックサムの仕様策定と実装
+	// frame.data_hash;
+	frame.data_size = datasize;
+	SetCRC(CDFS::CastToFrame(frame));
+	WriteToStream(stream, CDFS::CastToFrame(frame));
+	++frameindex;
+}
+void CDFSBuilder::WriteDATAFrame(std::ostream& stream, const ContainsType& data)
+{
+	CDFSFrame frame;
+	frame.seq = uint64_t(frameindex);
+	frame.frametype = CDFSFrameTypes::DATA;
+	frame.data = data;
+	SetCRC(frame);
+	WriteToStream(stream, frame);
+	++frameindex;
+	datasize += 240U;
+}
+void CDFSBuilder::WriteCONTFrame(std::ostream& stream)
+{
+	CDFSCONTFrame frame;
+	frame.seq = uint64_t(frameindex);
+	frame.frametype = CDFSFrameTypes::CONT;
+	frame.data_current = frameindex;
+	{
+		auto si = label.cbegin();
+		auto di = frame.data_label.begin();
+		auto se = label.cend();
+		auto de = frame.data_label.end();
+		while((si != se)&&(di != de)) { *(di++) = *(si++); }
+	}
+	SetCRC(CDFS::CastToFrame(frame));
+	WriteToStream(stream, CDFS::CastToFrame(frame));
+	++frameindex;
+}
+
+void CDFSBuilder::WriteToStream(std::ostream& stream, const CDFSFrame& frame)
+{
+	auto sentry = std::ostream::sentry(stream);
+	if (bool(sentry))
+	{
+		stream.write((const std::ostream::char_type*)&frame, sizeof(CDFSFrame));
+	}
+}
+void CDFSBuilder::SetCRC(CDFSFrame& frame)
+{
+	auto checksum = CRC32();
+	checksum.Push((const uint8_t*)&frame, (const uint8_t*)&frame.checksum);
+	frame.checksum = checksum.GetValue();
+}
