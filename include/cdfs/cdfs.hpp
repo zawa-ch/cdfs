19db62f267b7c9b9b9c1808f3483b1dbf2de9fa3
zawa-ch <satellite.2e1834097@gmail.com>
2020-10-22 22:57:41 +0900

    * cdfs/cdfs
    ドキュメント追加

--- include/cdfs/cdfs.hpp
+++ include/cdfs/cdfs.hpp
@@ -1,15 +1,21 @@
+//	cdfs/cdfs
+//	Copyright 2020 zawa-ch.
+//
 #ifndef __cdfs_cdfs__
 #define __cdfs_cdfs__
 #include "datatype.hpp"
 #include "checksum.hpp"
 namespace zawa_ch::CDFS
 {
+	///	CDFSライブラリの情報を提供します。
 	class CDFS final
 	{
 		CDFS() = delete;
 		~CDFS() = delete;
 	public:
+		///	対応しているCDFSのバージョン。
 		static constexpr uint32_t FormatVersion = 0x00000100;
+		///	CDFSライブラリのバージョンを取得します。
 		static uint32_t GetLibraryVersion() noexcept;
 	};
 }
